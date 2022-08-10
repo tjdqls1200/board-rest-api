b9e3dad7ceff822cd48e76ec19dfca5dd6e3fcea
Hyeonbo <hyeonbo.dev@gmail.com>
2022-08-10 23:05:43 +0900

    git init

---
+++ src/main/java/com/fivefingers/boardrestapi/BoardRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fivefingers.boardrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fivefingers/boardrestapi/BoardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivefingers.boardrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
