e29bc51c0a20d7aa8baa2cee44fd6d7236281bc3
code-tree <dltjdqls1200@naver.com>
2022-08-10 23:52:23 +0900

    BoardController 생성
    서버 정상적으로 실행되는지 확인

---
+++ src/main/java/com/fivefingers/boardrestapi/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.fivefingers.boardrestapi.controller;
+
+import com.fivefingers.boardrestapi.domain.Board;
+import com.fivefingers.boardrestapi.domain.BoardDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardDto> readPost(@PathVariable long id) {
+        // Service.findById(id)
+        Board board = Board.createBoard(id, "제목", "내용");
+        BoardDto boardDto = BoardDto.builder()
+                .id(board.getId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .build();
+        return ResponseEntity.ok().body(boardDto);
+    }
+}
